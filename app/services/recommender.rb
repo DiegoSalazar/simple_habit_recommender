5324da253d52069863aeea0d5e9c2452afd3c7e6
Diego Salazar <diego@greyrobot.com>
2017-01-24 23:13:12 -0500

    refactor algo

--- app/services/recommender.rb
+++ app/services/recommender.rb
@@ -7,29 +7,26 @@ class Recommender
     if (r = Recommendation.find_by_subtopic_id @subtopic_id)
       r.recommended_subtopic_ids
     else
-      s = get_sub_topics
-      binding.pry # debug
-      create_recommendation s
-      s
+      sub_topics = get_sub_topics
+      create_recommendation_for sub_topics
+      sub_topics
     end
   end
 
   private
 
   def get_sub_topics
-    binding.pry # debug
     User.who_listened_to(@subtopic_id).inject({}) do |sub_topics, user|
       user.listens.each do |listen|
         next if listen.subtopic_id == @subtopic_id
         sub_topics[listen.subtopic_id] ||= []
         sub_topics[listen.subtopic_id] << listen
       end
-      binding.pry # debug
       sub_topics
     end.sort_by { |_, listens| listens.size }.reverse
   end
 
-  def create_recommendation(sub_topics)
+  def create_recommendation_for(sub_topics)
     Recommendation.create({
       subtopic_id: @subtopic_id,
       recommended_subtopic_ids: sub_topics.map { |sid, listens| [sid, listens.size] }
